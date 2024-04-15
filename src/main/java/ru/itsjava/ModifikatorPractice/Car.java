cf2b325d99d4e3ae6e7ea8388cf90216e2615617
Gresenko Igor <fafafigor24@gmail.com>
2024-04-15 20:17:56 +0300

    new

---
+++ src/main/java/ru/itsjava/ModifikatorPractice/Car.java
@@ -0,0 +1,29 @@
+package ru.itsjava.ModifikatorPractice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@RequiredArgsConstructor
+public class Car {
+
+    private String brand;
+    private String color;
+    private static double price = 40000;
+
+
+
+    public void setPrice (double price1){
+        price=price1;
+    }
+
+
+
+    public double getPrice () {
+        return price;
+    }
+
+
+}
